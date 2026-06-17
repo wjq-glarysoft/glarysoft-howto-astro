6d5842625827ab268de34d4757ba71b4ba0e6419
wjq <1003056522@qq.com>
2026-06-17 20:02:36 +0800

    修改main.css谷歌翻译样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-// @ts-check
-import { defineConfig, fontProviders  } from 'astro/config';
+// astro.config.min.mjs
+import { defineConfig } from 'astro/config';
 
-import mdx from '@astrojs/mdx';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  output: 'static',
+  build: {
+    outDir: './dist',
+  },
 });
